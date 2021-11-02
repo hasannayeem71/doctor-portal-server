91fa8dd16e25ea78fb76e8a8d380ae40de8b8497
hasannayeem71 <2052cse00867@manarat.ac.bd>
2021-11-03 00:20:21 +0600

    conneected to database

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
+//middle ware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4mqcd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Doctor-Db");
+    const doctorsCollections = database.collection("doctors");
+    console.log("database connected successfully");
+  } finally {
+    //await client.close()
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Backend ok");
 });
